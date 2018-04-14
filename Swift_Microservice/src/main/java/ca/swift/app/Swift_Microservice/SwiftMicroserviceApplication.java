79f086ddd200574505283281de0411fdeae0473e
godwin gorgon <godwingm@live.com>
2018-04-14 16:26:31 -0400

    initial commit
    template

---
+++ Swift_Microservice/src/main/java/ca/swift/app/Swift_Microservice/SwiftMicroserviceApplication.java
@@ -0,0 +1,12 @@
+package ca.swift.app.Swift_Microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwiftMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwiftMicroserviceApplication.class, args);
+	}
+}
